03c4b957f1ec1637383fed659444aca90d4b9221
Noritaka Kamiya <noritakakamiya@gmail.com>
2020-03-07 16:25:46 +0900

    initial commit

---
+++ src/main/kotlin/com/noritakakamiya/propertyendpointdemo/PropertyEndpointDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.noritakakamiya.propertyendpointdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PropertyEndpointDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<PropertyEndpointDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/noritakakamiya/propertyendpointdemo/PropertyEndpointDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.noritakakamiya.propertyendpointdemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PropertyEndpointDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
